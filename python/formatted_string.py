d46df9bfcc70a324e3ebd7cdaa09b56dde8a0f14
Kishore Kumar Behera <kishore.behera2010@gmail.com>
2023-02-23 08:22:30 +0530

    Initial Commit

---
+++ python/formatted_string.py
@@ -0,0 +1,9 @@
+first_name = "kishore"
+second_name = "kumar"
+last_name = "behera"
+
+full_name = first_name + " " + second_name + " " + last_name
+print(full_name)
+
+# Formatted String
+print(f"{first_name} {second_name} {last_name}")
---
+++ python/type_conv.py
@@ -0,0 +1,3 @@
+x = int(input("Enter X: "))
+y = x + 1
+print(f"x: {x}, y: {y}")
